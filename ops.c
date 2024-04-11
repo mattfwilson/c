f6328660e52a265ece07933f7ecae1162949587a
mattfwilson <mattfwilson@gmail.com>
2024-04-11 00:36:02 -0400

    Program to calculate your weight on different planets

--- ops.c
+++ ops.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int x = 9;
-    int y = 2;
-    int z = 3;
-    int mod_x_y = (x + z) / y;
+    float x = 8;
+    float y = 2;
+    float z = 3;
+    float mod_x_y = (x + z) / y;
 
-    printf("x + y / z = %d\n", mod_x_y);
+    printf("x + y / z = %f\n", mod_x_y);
     
     if (x > y) {
-        printf("%d is greater than y.", x);
+        printf("%f is greater than y.", x);
     } else if (x < y) {
-        printf("%d is less than y.", x);
+        printf("%f is less than y.", x);
     } else {
-        printf("%d is equal to %d.", x, y);
+        printf("%f is equal to %f.", x, y);
     }
 }
---
+++ planet_weights.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  double weight;
+  int planet;
+
+  printf("What is your weight? ");
+  scanf("%lf", &weight);
+
+  printf("Which planet are you visiting? \n");
+  printf("\t1. Mercury\t2. Venus\t3. Mars\t4. Jupiter\t5. Saturn\t6. Uranus\t7. Neptune\n\n");
+  scanf("%d", &planet);
+
+  if (planet == 1) {
+    weight = weight * 0.38;
+  } else if (planet == 2) {
+    weight = weight * .91;
+  } else if (planet == 3) {
+    weight = weight * .38;
+  } else if (planet == 4) {
+    weight = weight * 2.34;
+  } else if (planet == 5) {
+    weight  = weight * 1.06;
+  } else if (planet == 6) {
+    weight = weight * .92;
+  } else if (planet == 7) {
+    weight = weight *1.19;
+  }
+ 
+  float rounded_weight = round(weight * 100) / 100;
+  printf("Your weight on the destination planet is is %f", rounded_weight);
+
+  return 0;
+}
