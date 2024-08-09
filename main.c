4d77071284ab5a3e57ba5f71aa754e915cf54b61
mattfwilson <mattfwilson@gmail.com>
2024-08-09 02:22:53 -0400

    Updated args

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Program name (argument 0) is: %s\n", argv[0]);
+  printf("Program name (Argument 0) is: %s\n", argv[0]);
 
-  if (argc > 0) {
+  if (argc > 1) {
     for (int i = 1; i < argc; i++) {
-      printf("Argument is %d: %s\n", i, argv[i]);
+      printf("Argument %d is: %s\n", i, argv[i]);
     }
   }
 
   else {
-    printf("No other args passed in.\n");
+    printf("No other arguments passed in.\n");
   }
 
   return 0;
