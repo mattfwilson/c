a5f408f5105909837629ca13e21ff79abbd7b737
mattfwilson <mattfwilson@gmail.com>
2024-08-11 12:56:13 -0400

    Printing uninitialized arrays

--- arrays.c
+++ arrays.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
 int main() {
-  float float_arr[5];
-  int int_arr[] = {10, 0, 50, 70, 90};
-  int size = sizeof(int_arr) / sizeof(int_arr[0]);
+  float float_arr[5] = {0.0};
+  float float_arr2[5];
+  int int_arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+  int int_size = sizeof(int_arr) / sizeof(int_arr[0]);
  
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < 5; i++) {
+    printf("%f", float_arr[i]);
+  }
+  printf("\n");
+
+  for (int i = 0; i < 5; i++) {
+    printf("%f ", float_arr2[i]);
+  }
+  printf("\n");
+
+  printf("[Size of array: %d]\n", int_size);
+  for (int i = 0; i < int_size; i++) {
     printf("%d ", int_arr[i]);
   }
-
   printf("\n");
+
   return 0;
 }
