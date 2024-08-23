2668a889105548bb6fd2e5b1973859ae02162057
mattfwilson <mattfwilson@gmail.com>
2024-08-23 12:55:05 -0400

    Created second func to accept char arrays printing out both ascii and char values

--- arr_test.c
+++ arr_test.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
-void test(int* arr, int size) {
+
+void show_int_arr(int* arr, int size) {
   for (int i = 0; i < size; i++) {
-    printf("%d - %d > %d\n", *(arr + i), arr, arr + i);
+    printf("index %d, address %d, value %d\n", i, arr + i, *(arr + i));
   }
 }
 
+void show_char_arr(char* arr, int size) {
+  for (int i = 0; i < size; i++) {
+    printf("index %d, address %d, ascii value %d, char value %c\n", i, arr + i, *(arr + i), *(arr + i));
+  }
+}
+
+
 int main() {
-  int arr[] = {1, 2, 3, 4, 5};
+  int arr[] = {3, 27, 9, 16, 22};
+  char arr2[] = {"Hello"};
   int size = sizeof(arr) / sizeof(int);
 
-  test(arr, size);
-  
+  show_int_arr(arr, size);
+  printf("\n");
+  show_char_arr(arr2, size);
+
   return 0;
 }
