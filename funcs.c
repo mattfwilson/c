3f8dc54f60d988af300c4fb28cc52b8e52dd7567
mattfwilson <mattfwilson@gmail.com>
2024-09-04 00:22:38 -0400

    Rand func

--- funcs.c
+++ funcs.c
@@ -5,7 +5,9 @@
 int main() {
   const char *to_int = "Hi pal!";
   long num = atol(to_int);
+  int randNum = rand();
 
-  printf("%s equates to %i as an integer", to_int, num);
+  printf("%s equates to %i as an integer\n", to_int, num);
+  printf("%i", randNum);
   return 0;
 }
