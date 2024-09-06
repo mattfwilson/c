9d5ac814240ba179b8f32d880a374aab3b29f06e
mattfwilson <mattfwilson@gmail.com>
2024-09-06 01:02:00 -0400

    malloc pointer

--- funcs.c
+++ funcs.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 int main() {
   const char *to_int = "Hi pal!";
   long num = atol(to_int);
   int randNum = rand();
+  int *ptr = (int *)malloc(sizeof(int));
 
   printf("%s equates to %i as an integer\n", to_int, num);
-  printf("%i", randNum);
+  printf("%i\n", randNum);
+  printf("%p", *ptr);
   return 0;
 }
