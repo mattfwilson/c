391babde02f177c5c37bdac7773e4366e95ee4d6
mattfwilson <mattfwilson@gmail.com>
2024-09-23 00:11:13 -0400

    Fixed decimal output display

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -3,15 +3,15 @@
 int main() {
   for (int i = 0; i <= 100; i++) {
     if (i % 3 == 0) {
-      printf("%d is divisible by 3.", i);
+      printf("%d is divisible by 3.\n", i);
     } 
     else if (i % 5 == 0) {
-      printf("%d is divisible by 5.", i);
+      printf("%d is divisible by 5.\n", i);
     }
     else if (i % 3 == 0 && i % 5 == 0) {
-      printf("%d is divisible by both 3 and 5.", i);
+      printf("%d is divisible by both 3 and 5.\n", i);
     }
-    printf("%d\n", i);
+    //printf("%d\n", i);
   }
   return 0;
 }
