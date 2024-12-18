3f66164344df58e8c108dfc5184f775a1950ec9e
mattfwilson <mattfwilson@gmail.com>
2024-12-18 00:29:44 -0500

    Codecademy calendar

---
+++ cal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool is_leap_year(int year) {
+  if (year > 2000) {
+    printf("%d is greater than 2000");
+    return true;
+  printf("%d is less than 2000");
+  return false;
+  }
+}
+
+
+int main() {
+  int year1 = 2001;
+  int year2 = 1999;
+
+  main(year1);
+
+  return 0;
+}
+
+
