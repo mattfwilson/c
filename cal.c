444485a4375cf5ad40055a10f49ccc40028609b4
mattfwilson <mattfwilson@gmail.com>
2025-03-18 00:45:11 -0400

    Added array of day counts for months

--- cal.c
+++ cal.c
@@ -4,11 +4,11 @@
 
 bool is_leap_year(int year) {
   if (year > 2000) {
-    printf("%d is greater than 2000", year);
+    printf("%d is greater than 2000\n", year);
     return true;
   }
   else {
-    printf("%i is less than 2000", year);
+    printf("%i is less than 2000\n", year);
     return false;
   }
 }
@@ -20,6 +20,25 @@ int main() {
 
   is_leap_year(year1);
 
+
+  int days_in_month[13] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12};
+  days_in_month[0] = 31;
+  days_in_month[1] = 31;
+  days_in_month[2] = 28;
+  days_in_month[3] = 31;
+  days_in_month[4] = 30;
+  days_in_month[5] = 31;
+  days_in_month[6] = 30;
+  days_in_month[7] = 31;
+  days_in_month[8] = 31;
+  days_in_month[9] = 30;
+  days_in_month[10] = 31;
+  days_in_month[11] = 31;
+  days_in_month[12] = 31;
+
+  for (int i = 0; i < 13; i++) {
+    printf("%d\n", days_in_month[i]);
+  }
   return 0;
 }
 
