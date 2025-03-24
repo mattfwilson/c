72414bef7c8efbb9a839e8a6092cc6d14a9b9a80
mattfwilson <mattfwilson@gmail.com>
2025-03-24 13:49:41 -0400

    Second versions of rps. Passing choices array into play func

---
+++ rps2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void play(const char *arr[], int size) {
+  srand(time(NULL));
+  int rand_index = rand() % size;
+  printf("CPU chose %s!\n", arr[rand_index]);
+  
+  for (int i; i < size; i++) {
+    printf("%s\n", arr[i]);
+  }
+}
+
+
+int main() {
+  const char *choices[] = {"Rock", "Paper", "Scissors", "Shotgun"};
+  int size = sizeof(choices) / sizeof(choices[0]);
+  char choice[64];
+
+  printf("Enter choice: ");
+  scanf("%s", &choice);
+
+  int found;
+  found = 0;
+
+  for (int i; i < size; i++) {
+    if (strcmp(choice, choices[i]) == 0) {
+      found = 1;
+      break;
+      }
+  }
+
+  if (found) {
+    printf("%s is not valid.", choice);
+  }
+    else {
+    printf("You chose %s!\n", choice);
+    play(choices, size);
+  }
+
+  return 0;
+}
