5d63e60a3d964efc3a30f22745535da7b89cb9d5
Matthew Wilson <mattfwilson@gmail.com>
2025-04-06 00:22:24 -0400

    Struct learning

---
+++ struct.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Card {
+  char name[75];
+  int energy;
+  char type[75];
+  int attack;
+  int block;
+};
+
+int main() {
+  struct Card c1;
+  strcpy(c1.name, "Strike");
+  c1.energy = 2;
+  strcpy(c1.type, "Attack");
+  c1.attack = 6;
+  c1.block = 0;
+
+  printf("name:     %s\n", c1.name);
+  printf("type:     %s\n", c1.type);
+  printf("energy:   %d\n", c1.energy);
+  printf("attack:   %d\n", c1.attack);
+  printf("block:    %d\n", c1.block);
+
+  struct Card c2 = {
+    "STRIKE",
+    4,
+    "ATTACK",
+    3,
+    3
+  };
+
+  printf("name:     %s\n", c2.name);
+  printf("type:     %s\n", c2.type);
+  printf("energy:   %d\n", c2.energy);
+  printf("attack:   %d\n", c2.attack);
+  printf("block:    %d\n", c2.block);
+
+  return 0;
+}
