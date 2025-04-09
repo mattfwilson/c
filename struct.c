ca28ea48d73a897d36ac519a6e66532aa4625b85
Matthew Wilson <mattfwilson@gmail.com>
2025-04-09 00:24:37 -0400

    dot notation and arrow notation with struct pointers

--- struct.c
+++ struct.c
@@ -17,19 +17,28 @@ int main() {
   c1.attack = 6;
   c1.block = 0;
 
+  printf("Card 1    ----------------\n");
   printf("name:     %s\n", c1.name);
   printf("type:     %s\n", c1.type);
   printf("energy:   %d\n", c1.energy);
   printf("attack:   %d\n", c1.attack);
   printf("block:    %d\n\n", c1.block);
 
+  printf("Card 2    ----------------\n");
   struct Card c2 = {"Defend", 1, "Block", 0, 5};
+  printf("energy:   %d\n", c2.energy);
+
+  struct Card* c2Pointer = &c2;
+  (*c2Pointer).energy = 10;
 
   printf("name:     %s\n", c2.name);
   printf("type:     %s\n", c2.type);
   printf("energy:   %d\n", c2.energy);
   printf("attack:   %d\n", c2.attack);
   printf("block:    %d\n", c2.block);
+
+  c2Pointer -> energy = 3;
+  printf("energy:   %d\n", c2.energy);
 
   return 0;
 }
