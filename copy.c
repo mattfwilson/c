c9d642efef1ba03ccfa62c972c198bddcd216f6d
mattfwilson <mattfwilson@gmail.com>
2025-05-30 10:17:45 -0400

    Reviewing string copying

--- copy.c
+++ copy.c
@@ -5,15 +5,18 @@ void copy(char* dst, char* src) {
   while (*src != '\0') {
     *dst = *src;
     src++;
+    printf("%s\n", src);
     dst++;
+    printf("%s\n", dst);
   }
   *dst = '\0';
 }
  
 int main(){
-  char srcString[] = "We promptly judged antique ivory buckles for the next prize!";
-  char dstString[strlen(srcString)] = {};
+  char srcString[] = "Hello World";
+  char dstString[strlen(srcString) + 1];
 
   copy(dstString, srcString);
+  printf("%s\n", srcString);
   printf("%s", dstString);
 }
