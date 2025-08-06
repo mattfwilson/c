20668e0255c0bb30ffddd25adccd816535498223
mattfwilson <mattfwilson@gmail.com>
2025-08-05 23:09:01 -0400

    Cleaned up output

--- pass_arr.c
+++ pass_arr.c
@@ -3,8 +3,7 @@
 void printArr(int array[], int size) {
   for (int i = 0; i < size; i++) {
     array[i]++;
-    printf("%d ", array[i]);
-    printf("%p", &array[i]);
+    printf("%d (%p)\n", array[i], (void*)&array[i]);
   }
 }
 
@@ -17,17 +16,15 @@ int main() {
     printf("%d ", coffeePerWeek[i]);
   }
 
-  printf("\n");
-  printArr(coffeePerWeek, arr_size); // arrays passed into funcions decay to a pointer; and pointers do not hold a size
   printf("\n\n");
+
+  printf("After printArr:\n");
+  printArr(coffeePerWeek, arr_size);
+  printf("\n");
   
   for (int i = 0; i < arr_size; i++) {
     printf("%d ", coffeePerWeek[i]);
   }
-
-  printf("\n");
-  printArr(coffeePerWeek, arr_size);
-  printf("\n\n");
 
   return 0;
 }
