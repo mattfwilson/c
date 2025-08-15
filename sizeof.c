e692244dc6acff66818b2058d637361f124bdcc5
mattfwilson <mattfwilson@gmail.com>
2025-08-15 01:39:02 -0400

    Conditional to gauge size of arr length

--- sizeof.c
+++ sizeof.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 void print_size(int arr[], int len) {
-   printf("array len: %d\n", len);
+  if (len > 10) {
+    printf("array len: %d, HUGE", len);
+  } else {
+    printf("array len: %d, tiny...", len);
+  }
 }
 
 int main() {
-  int numArr[] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21};
+  int numArr[] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
   int arrSize = sizeof(numArr) / sizeof(int);
   print_size(numArr, arrSize);
 
