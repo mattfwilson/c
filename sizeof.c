3cc7ea73d5b49de963161c3053488f4b0df2de0e
mattfwilson <mattfwilson@gmail.com>
2025-08-17 01:53:12 -0400

    Returning size of array from func

--- sizeof.c
+++ sizeof.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-void print_size(int arr[], int len) {
+int print_size(int arr[], int len) {
   if (len > 10) {
     printf("array len: %d, HUGE", len);
+    return len;
   } else {
     printf("array len: %d, tiny...", len);
-    printf("\narray: %d", arr);
+    return len;
   }
 }
 
 int main() {
   int numArr[] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
   int arrSize = sizeof(numArr) / sizeof(int);
-  print_size(numArr, arrSize);
+  int res = print_size(numArr, arrSize);
+  printf("\n%d", res);
 
   return 0;
 }
