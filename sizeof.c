168489abdd6b45df6935dd370b8de70528face61
mattfwilson <mattfwilson@gmail.com>
2025-08-18 09:43:04 -0400

    Passing array size pointer into func

--- sizeof.c
+++ sizeof.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int print_size(int arr[], int len) {
+int print_size(int arr[], int len, int *arrPtr) {
   if (len > 10) {
-    printf("array len: %d, HUGE", len);
-    return len;
+    printf("array len %d stored at: %p, HUGE", len, arrPtr);
   } else {
-    printf("array len: %d, tiny...", len);
-    return len;
+    printf("array len %d stored at: %p, tiny...", len, arrPtr);
   }
+  return len;
 }
 
 int main() {
   int numArr[] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
   int arrSize = sizeof(numArr) / sizeof(int);
-  int res = print_size(numArr, arrSize);
+  int *arrPtr = &arrSize;
+  int res = print_size(numArr, arrSize, arrPtr);
   printf("\n%d", res);
 
   return 0;
