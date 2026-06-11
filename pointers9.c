5ff35538c4814b266f9ac36a3144a5c015c7884b
mattfwilson <mattfwilson@gmail.com>
2026-06-11 16:22:30 -0400

    Pointer arithmetic

--- pointers9.c
+++ pointers9.c
@@ -8,10 +8,18 @@ int main() {
   int *ptr = &num;
   int **ptr = &ptr;
 
+  int arr[7] = {0, 1, 2, 3, 5, 7, 13};
+
+
   printf("Var num: %d\n", num);
   printf("Memory address %p\n", &num);
   printf("Var num pointer dereferenced: %d\n", *ptr);
-  printf("Pointer of pointer %d\n", **ptr);
+  printf("Pointer of pointer %d\n", *ptr);
+  
+  printf("%d\n", *arr);
+  printf("%d\n", *(arr + 1));
+  printf("%d\n", *(arr + 2));
+  printf("%d\n", *(arr + 3));
 
   return 0;
 }
