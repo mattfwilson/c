f6caf8df8c942e2647d694663d0e1a5d01ac52cf
mattfwilson <mattfwilson@gmail.com>
2026-06-12 18:15:24 -0400

    Setup clang/gcc and fixed pointer of pointer bug

---
+++ _clangtest.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+int main(void){ printf("clang works! 2+2=0
+", 2+2); return 0; }
--- pointers9.c
+++ pointers9.c
@@ -6,15 +6,13 @@ int main() {
   int num;
   num = 4;
   int *ptr = &num;
-  int **ptr = &ptr;
-
+  int **pptr = &ptr;
   int arr[7] = {0, 1, 2, 3, 5, 7, 13};
 
-
   printf("Var num: %d\n", num);
-  printf("Memory address %p\n", &num);
-  printf("Var num pointer dereferenced: %d\n", *ptr);
-  printf("Pointer of pointer %d\n", *ptr);
+  printf("Address %p\n", (void *)&num);
+  printf("Pointer dereferenced: %d\n", *ptr);
+  printf("Pointer of pointer %d\n", **pptr);
   
   printf("%d\n", *arr);
   printf("%d\n", *(arr + 1));
