c3679514ca042add045bb0e93d10895d76cf7cec
mattfwilson <mattfwilson@gmail.com>
2026-07-03 13:16:49 -0400

    Pointing to arrays practice

---
+++ arr_pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int arr_len = sizeof(arr) / sizeof(arr[0]);
+    int i;
+
+    for (i = 0; i < arr_len; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    for (i = 0; i < arr_len; i++) {
+        printf("%d\n", arr[&i]);
+    }
+
+    return 0;
+}
+
