a3c250f2386f76df2018712ad9d0356e6fa7da34
mattfwilson <mattfwilson@gmail.com>
2026-07-05 23:03:28 -0400

    Looping with second iterator

--- arr_pointers.c
+++ arr_pointers.c
@@ -6,10 +6,11 @@ int main() {
     int arr[5] = {10, 20, 30, 40, 50};
     int arr_len = sizeof(arr) / sizeof(arr[0]);
     int i;
+    int j = 2;
 
-    for (i = 0; i < arr_len; i++) {
+    for (i = 0; j < arr_len; i++) {
         printf("%d\n", arr[i]);
-        printf("%p\n", &arr[i]);
+        printf("%p\n", (void *)&arr[i]);
     }
 
     return 0;
