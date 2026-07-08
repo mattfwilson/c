97f0f5abdfd8ad10c3b83a51b982702b627513d7
mattfwilson <mattfwilson@gmail.com>
2026-07-07 23:43:39 -0400

    While looping third iterator

--- arr_pointers.c
+++ arr_pointers.c
@@ -7,11 +7,17 @@ int main() {
     int arr_len = sizeof(arr) / sizeof(arr[0]);
     int i;
     int j = 2;
+    int k = 0;
 
     for (i = 1 + j; j < arr_len; j++) {
         printf("array item: %d\n", arr[i]);
         printf("array item address: %p\n", (void *)&arr[i]);
         printf("full array address: %p\n", &arr);
+    }
+    
+    while (k < 5) {
+        printf("%d is still less than 5\n", k);
+        k++;
     }
 
     return 0;
